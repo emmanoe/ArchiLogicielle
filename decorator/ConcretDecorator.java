e887f5d76dc012aece8b3754ba2a18e28ccc2245
Emmanoe DELAR <26841656+emmanoe@users.noreply.github.com>
2020-03-10 14:58:57 +0100

    Finishing TP5 Decorator Hommework

--- decorator/ConcretDecorator.java
+++ decorator/ConcretDecorator.java
@@ -1,17 +1,25 @@
 
-public class ShieldDecorator extends Decorator
+public class ConcretDecorator extends Decorator
 {
 	private float _shield;
+	private float _sword;
 	
-	ShieldDecorator(Soldat soldat, float shield)
+	ConcretDecorator(Soldat soldat, float shield)
 	{
 		super(soldat);
 		_shield = shield;
 	}
 	
+	@Override
 	public void parry(float strikeForce)
 	{
 		_soldat.parry(strikeForce - _shield);
+	}
+	
+	@Override
+	public float strike()
+	{
+		return _soldat.strike() + _sword;
 	}
 
 	@Override
--- decorator/Main.java
+++ decorator/Main.java
@@ -2,8 +2,8 @@
 public class Main {
 
 	public static void main(String[] args) {
-		Soldat s1 = new ShieldDecorator(new SwordDecorator(new HumanSoldier("infantryman"), 100),50);
-		Soldat s2 = new ShieldDecorator(new SwordDecorator(new HumanSoldier("horseman"), 100),1000);
+		Soldat s1 = new ConcretDecorator(new ConcretDecorator(new HumanSoldier("infantryman"), 100),50);
+		Soldat s2 = new ConcretDecorator(new ConcretDecorator(new HumanSoldier("horseman"), 100),1000);
 		
 		Fight f = new Fight();
 		f.fight(s1,s2);
